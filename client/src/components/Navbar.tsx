a63dce6262614534603b315a39050189187f212c
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-04-22 15:12:15 +0200

    feat: create navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router";
+import favicon from "../assets/images/favicon.svg";
+
+useNavigate;
+
+function NavBar() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="navbar">
+      <button type="button" className="fav_nav" onClick={() => navigate("/")}>
+        <img src={favicon} alt="nav-favicon" />
+      </button>
+    </div>
+  );
+}
+
+export default NavBar;
