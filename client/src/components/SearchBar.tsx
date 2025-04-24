ef6517dd661ed8813861226246d8befea70da6e7
le-Faucon09 <jerome.wiera@live.fr>
2025-04-24 09:56:02 +0200

    feat: search gameData

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,19 +1,53 @@
-/*import React, { useEffect, useState } from 'react';*/
+import type React from "react";
+import { useState } from "react";
 import loupe_search from "../assets/images/loupe_search.svg";
 
+type Game = {
+  id: number;
+  title: string;
+};
+
+const gameData: Game[] = [
+  { id: 1, title: "FIFA 25" },
+  { id: 2, title: "Call of Duty" },
+  { id: 3, title: "Zelda" },
+];
 function SearchBar() {
-  /*const [data, setDatas] = useState([]);
-    console.log(setDatas, data)*/
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleSearchTerm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length > 1) {
+      setSearchTerm(value);
+    } else {
+      setSearchTerm("");
+    }
+  };
+
   return (
-    <div className="Search">
-      <input
-        type="text"
-        placeholder="Recherchez un jeux"
-        name="SearchBar"
-        id="SearchBar"
-      />
-      <img src={loupe_search} alt="loupe" />
-    </div>
+    <>
+      <div className="SearchBar">
+        <input
+          type="text"
+          placeholder="Recherchez un jeu"
+          name="SearchBar"
+          id="SearchBar"
+          onChange={handleSearchTerm}
+        />
+        <img src={loupe_search} alt="loupe" />
+      </div>
+      <div className="search_result">
+        {gameData
+          .filter((val) =>
+            val.title.toLowerCase().includes(searchTerm.toLowerCase()),
+          )
+          .map((val) => (
+            <div className="search_result_item" key={val.id}>
+              {val.title}
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
 
