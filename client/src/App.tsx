b4ab8946283f0d11fc1b9a8f297c0cfa3c40f1b3
le-Faucon09 <jerome.wiera@live.fr>
2025-04-24 13:30:50 +0200

    feat: topgames

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import GameCard from "./components/GameCard";
 import NavBar from "./components/Navbar";
+import TopGames from "./components/TopGames";
 
 function App() {
   return (
     <>
       <NavBar />
       <GameCard />
+      <TopGames />
     </>
   );
 }
---
+++ client/src/components/TopGames.tsx
@@ -0,0 +1,10 @@
+function TopGames () {
+    
+return (
+    <div>
+    <h1>Les mieux notés</h1>
+    </div>
+)
+}
+
+export default TopGames
