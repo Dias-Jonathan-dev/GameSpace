e42b3425e3905734222b1d169f0bf64bf093516f
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-04-25 11:04:34 +0200

    feat: add header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import GameCard from "./components/GameCard";
+import Header from "./components/Header";
 import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Header />
       <GameCard />
     </>
   );
