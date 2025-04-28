d97c4c9fbfdc2aafe966d03dc5b9e0f0088863ee
zarzour <ridouane@MacBook-Air-de-ridouane.local>
2025-04-28 10:02:18 +0200

    feat: create page 404 component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Footer from "./components/Footer";
 import GameCard from "./components/GameCard";
 import NavBar from "./components/Navbar";
-import Page404 from "./components/Page404";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <NavBar />
       <GameCard />
       <Footer />
-      <Page404 />
     </>
   );
 }
