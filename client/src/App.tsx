8685a2366db78af017b111c0c7d1e8b83f8f682d
jonathan dias <jonathan-dias@outlook.fr>
2025-05-02 14:58:48 +0200

    style: improve css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import "../src/services/Reset.css";
 import "./App.css";
 import Categories from "./components/Category";
 import Footer from "./components/Footer";
-import GameCardsArea from "./components/GameCardsArea";
 import Header from "./components/Header";
 import NavBar from "./components/Navbar";
 import NewSection from "./components/NewSection";
@@ -16,7 +15,6 @@ function App() {
       <NewSection />
       <Categories />
       <TopGames />
-      <GameCardsArea />
       <Footer />
     </>
   );
