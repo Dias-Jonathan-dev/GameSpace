346de5984cf784e87ce0646571dd092e86f171db
jonathan dias <jonathan-dias@outlook.fr>
2025-05-02 14:59:02 +0200

    style: improve css

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,11 +5,11 @@ function Header() {
     <header>
       <img src={LogoBlanc} alt="logo-gamespace" className="header-img" />
       <div className="header-text">
-         <p>
-          Bienvenue sur <span className="text-orange">GameSpace</span>, ton nouveau terrain de jeu numérique !
-          Découvre les dernières sorties, explore tous les styles de jeux, et
-          trouve ta prochaine obsession vidéoludique parmi des centaines de
-          titres triés sur le volet !
+        <p>
+          Bienvenue sur <span className="text-orange">GameSpace</span>, ton
+          nouveau terrain de jeu numérique ! Découvre les dernières sorties,
+          explore tous les styles de jeux, et trouve ta prochaine obsession
+          vidéoludique parmi des centaines de titres triés sur le volet !
         </p>
       </div>
     </header>
