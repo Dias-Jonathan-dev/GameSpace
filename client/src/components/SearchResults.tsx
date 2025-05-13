6ddabb7c9b8af8e0ad9cdb10fa94f126f1ff088e
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-05-13 16:13:20 +0200

    feat: improve news and searchbar

---
+++ client/src/components/SearchResults.tsx
@@ -0,0 +1,26 @@
+import GameCard from "./GameCard";
+import type { Game } from "../services/UseFetchGames";
+
+interface SearchResultsProps {
+  searchTerm: string;
+  games: Game[];
+}
+
+function SearchResults({ searchTerm, games }: SearchResultsProps) {
+  const filteredGames = games.filter((game: Game) =>
+    searchTerm
+      ? game.name.toLowerCase().includes(searchTerm.toLowerCase())
+      : false,
+  );
+  return filteredGames.length ? (
+    <div className="games-cards">
+      {filteredGames.map((game: Game) => (
+        <div key={game.id}>
+          <GameCard game={game} />
+        </div>
+      ))}
+    </div>
+  ) : null;
+}
+
+export default SearchResults;
